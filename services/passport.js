c2ca09093c0b72021e8fc4e0156cedbd275e9a15
Kseniia Kuvaldina <kseniia@Kseniias-MacBook-Air.local>
2021-01-06 20:51:52 -0700

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -3,9 +3,6 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-
-
-
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
@@ -24,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
