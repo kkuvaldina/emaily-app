ce31c62a06ada3a472cfd02fb992116978136714
Kseniia Kuvaldina <kseniia@Kseniias-MacBook-Air.local>
2021-01-06 20:41:01 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+//340035991431-pa0inlh63ro915thrj2j15u0fcom9883.apps.googleusercontent.com
+//0a7Ha0Mazk1p19sMcLg6y2r1
+
+
+//mongodb+srv://user2:BPRgim5zO5r8C0xP>@cluster0.ah4kz.mongodb.net/emaily?retryWrites=true&w=majority
+
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
