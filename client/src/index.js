74fcda38c206638c3096330780a9791e84d03503
Kseniia Kuvaldina <kseniia@Kseniias-MacBook-Air.local>
2021-02-03 18:34:51 -0700

    user can see survey result

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -16,5 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.querySelector("#root")
 );
-
-console.log(process.env.REACT_APP_STRIPE_KEY);
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,14 @@
+const re = /^(([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)(\s*;\s*|\s*$))*$/;
+///^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default (emails) => {
+  const invalidEmails = emails
+    .split(",")
+    .map((email) => email.trim())
+    .filter((email) => re.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+  return;
+};
